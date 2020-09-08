22eef56f2607d760408de866f7d9dd31c48f64f2
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-09-08 15:04:43 +0530

    Add files via upload

---
+++ substraction.sh
@@ -0,0 +1,7 @@
+echo "this is substraction program in bash"
+echo "Enter a value"
+read a
+echo "Enter b value"
+read b
+c=`expr $a - $b`
+echo "the difference of given numbers is" $c                                                              
