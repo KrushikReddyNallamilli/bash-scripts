1e8e419bde85aa8eaa3c2e0201242f5ee1b6395c
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-09-14 15:13:40 +0530

    Update prime-fibonacci-numbers.sh

--- prime-fibonacci-numbers.sh
+++ prime-fibonacci-numbers.sh
@@ -5,28 +5,35 @@ read N
 a=0  
 b=1 
 
-echo "The Prime Fibonacci series is : "
+echo "The Fibonacci series is : "
 echo -n " $a   $b "
 for (( i=0; i<=N-2; i++ ))	
 do
 	
 	fn=$((a + b))
+	
+	
 	i=2 
+  
     f=0 
-    while test $i -le `expr $fn / 2 `  
+      
+    while test $i -le `expr $fn / 2`  
     do
-        if test `expr $fn % $i` -eq 0  
-        then
-            f=1 
-        fi
-        i=`expr $i + 1` 
+    if test `expr $fn % $i` -eq 0  
+    then
+    f=1 
+    fi
+
+    i=`expr $i + 1` 
     done
     if test $f -eq 1  
     then
     echo ""
     else
-    #primes
     echo -n " $fn "
     fi
-    	
+	
+	#echo -n " $fn "
+	a=$b 
+	b=$fn 
 done
