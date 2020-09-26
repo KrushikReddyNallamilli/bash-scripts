b3c58eaff4ce54ea8452441c77e491b2d0cb5d5e
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-09-26 14:52:55 +0530

    Add files via upload

--- numberpalindrome.sh
+++ numberpalindrome.sh
@@ -5,8 +5,8 @@ num1=$num
 reverse=0
 while [ $num -gt 0 ]
 do 
-	rem= `expr $num % 10 `
-	reverse= `expr $reverse \* 10 + $rem`
+	rem=`expr $num % 10`
+	reverse=`expr $reverse \* 10 + $rem`
 	num=`expr $num / 10`
 done
 if [ $num -eq $reverse ]
